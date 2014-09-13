2b1bc35def6e2e555e6affc464778bf5f1c69a90
Kirsten Jones <synedra@gmail.com>
2014-09-13 14:19:20 -0700

    Fixing hostname in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
 		db: 'mongodb://heroku7926883af05244d1c3a8b51@kahana.mongohq.com:10005/app29516722',
 		fitbitClientKey: '152d21adfadd4511a1915b47e63b0636', 
 		fitbitClientSecret: '7adb7e6d8e394cad9daacc1372fbfcfb',
-		host: 'http://young-woodland-4547.herokuapp.com' ,
+		host: 'young-woodland-4547.herokuapp.com' ,
                 twilioAccountSid: 'AC80f1b83ec7f84c4fbf7977330b503745',
                 twilioAuthToken: 'ac026657401b239fdce4d14c182820fe',
                 twilioPhoneNumber: '+1 831-216-1666'
